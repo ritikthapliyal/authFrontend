14e1ec232588eb4516464c4aa80cacb553c54960
ritikthapliyal <ritikthapliyal123@gmail.com>
2023-09-14 01:00:56 +0530

    fixed up-down button

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -60,8 +60,18 @@ function LoginPage() {
                                 placeholder='enter password'
                                 value={dataObj.password}
                                 onChange={(e)=>{setDataObj({...dataObj,password:e.target.value})}}></input>
-                                <Link>Forgot Password?</Link>
-                            <button onClick={loginUser}>Login</button>
+
+                                <div>
+                                    <Link>Forgot Password?</Link>
+                                    <button onClick={loginUser}>Login</button>
+                                </div>
+
+                                <button className='page-change-button' onClick={handleMove}>
+                                    register 
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3" />
+                                    </svg>
+                                </button>
 
                         </form>
                     </div>
@@ -100,20 +110,23 @@ function LoginPage() {
                                         value={dataObj2.password}
                                         onChange={(e)=>{setDataObj2({...dataObj2,password:e.target.value})}}></input>
                                 </div>
-                                
-                                <button onClick={registerUser}>register</button>
+
+                                <div>
+                                    <Link>Forgot Password?</Link>
+                                    <button onClick={registerUser}>register</button>
+                                </div>
+
+                                <button className='page-change-button' onClick={handleMove}>
+                                    Back to login
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75L12 3m0 0l3.75 3.75M12 3v18" />
+                                    </svg>
+                                </button>
 
                             </form>
                         </div>
                 </div>
             </div>
-
-            <button className='page-change-button' onClick={handleMove}>
-                register 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-                </svg>
-            </button>
 
         </div>
   )
